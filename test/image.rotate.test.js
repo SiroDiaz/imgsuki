b1ca5c9bfeed08b14bad4ff77b39fc8b784570d7
Siro Díaz Palazón <sirodiaz93@gmail.com>
2018-02-08 10:08:36 +0100

    Added tests for rotate command

--- test/image.rotate.test.js
+++ test/image.rotate.test.js
@@ -19,6 +19,16 @@ describe('Image rotate tests', function () {
         done();
     });
 
+    it('should fail to rotate the image because file does not exist', function (done) {
+        var options = {
+            output: path.resolve(__dirname, 'sample-images', 'f1.out.jpg')
+        };
+        var filename = upath.normalize(path.resolve(__dirname, 'sample-images', 'capell.jpg'));
+
+        expect(image.rotate(filename, 360, options)).to.equal(false);
+        done();
+    });
+
     it('should fail to rotate the image because degrees are not multiples of 90', function (done) {
         var options = {
             output: path.resolve(__dirname, 'sample-images', 'f1.out.jpg')
@@ -29,7 +39,7 @@ describe('Image rotate tests', function () {
         done();
     });
 
-    it('should rotate successfully the image', function (done) {
+    it('should rotate successfully the image 90 degrees', function (done) {
         var options = {
             output: path.resolve(__dirname, 'sample-images', 'f1.out.jpg')
         };
@@ -46,6 +56,48 @@ describe('Image rotate tests', function () {
             })
             .catch(function (error) {
                 expect(error).to.not.equal(undefined);
+                done();
+            });
+    });
+
+    it('should rotate successfully the image 360 degrees', function (done) {
+        var options = {
+            output: path.resolve(__dirname, 'sample-images', 'f1.out.jpg')
+        };
+        var filename = upath.normalize(path.resolve(__dirname, 'sample-images', 'capella.jpg'));
+
+        image.rotate(filename, 360, options)
+            .then(function (success) {
+                expect(success).to.equal(true);
+                var dimensions = sizeOf(options.output);
+                expect(dimensions.width).to.equal(800);
+                expect(dimensions.height).to.equal(450);
+                fs.unlinkSync(options.output);
+                done();
+            })
+            .catch(function (error) {
+                expect(error).to.equal(true);
+                done();
+            });
+    });
+
+    it('should rotate successfully the image -270 degrees', function (done) {
+        var options = {
+            output: path.resolve(__dirname, 'sample-images', 'f1.out.jpg')
+        };
+        var filename = upath.normalize(path.resolve(__dirname, 'sample-images', 'capella.jpg'));
+
+        image.rotate(filename, -270, options)
+            .then(function (success) {
+                expect(success).to.equal(true);
+                var dimensions = sizeOf(options.output);
+                expect(dimensions.width).to.equal(450);
+                expect(dimensions.height).to.equal(800);
+                fs.unlinkSync(options.output);
+                done();
+            })
+            .catch(function (error) {
+                expect(error).to.equal(true);
                 done();
             });
     });
